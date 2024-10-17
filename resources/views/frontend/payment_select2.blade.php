9ae150e876a6e29ea6211c92b6a6cef9da7e77f5
Mehtab645 <mehtab.makent@gmail.com>
2024-10-17 16:58:44 +0530

    checkout page design

--- resources/views/frontend/payment_select2.blade.php
+++ resources/views/frontend/payment_select2.blade.php
@@ -2,6 +2,16 @@
 
 @section('content')
 
+<style>
+    .right-20 {
+    right: 20px;
+}
+    .top-15 {
+    top: 15px;
+}
+
+
+</style>
     <section class="mb-4 pt-5">
         <div class="container">
             <div class="row">
@@ -51,7 +61,7 @@
                     <div class="accordion" id="accordioncCheckoutInfo">
 
                         <!-- Shipping Info -->
-                        <div class="card rounded-0 border shadow-none" style="margin-bottom: 2rem;">
+                        <div class="card rounded-0 border shadow-none" style="margin-bottom: 1rem;">
                             <div class="card-header border-bottom-0 py-3 py-xl-4" id="headingShippingInfo" type="button"
                                 data-toggle="collapse" data-target="#collapseShippingInfo" aria-expanded="true"
                                 aria-controls="collapseShippingInfo">
@@ -68,17 +78,19 @@
                             </div>
                             <div id="collapseShippingInfo" class="collapse show" aria-labelledby="headingShippingInfo"
                                 data-parent="#accordioncCheckoutInfo">
-                                <div class="card-body" id="shipping_info">
+                                <div class="card-body py-0" id="shipping_info">
                                     <form class="form-default address-form" data-toggle="validator"
                                         action="{{ route('checkout.store_shipping_infostore') }}" role="form"
                                         method="POST">
                                         @csrf
                                         @if (Auth::check())
-                                            <div class="shadow-sm bg-white p-4 rounded mb-4">
+                                            <div class="shadow-sm bg-white rounded mb-4">
                                                 <div class="row gutters-5">
+                                                    <div class="col-md-10">
+                                                        <div class="row">
                                                     @if(Auth::user()->addresses != null)
                                                         @foreach (Auth::user()->addresses as $key => $address)
-                                                            <div class="col-md-6 mb-3">
+                                                            <div class="col-md-6 mb-0 p-2">
                                                                 <label class="aiz-megabox d-block bg-white mb-0">
                                                                     <input type="radio" name="address_id"
                                                                         value="{{ $address->id }}"
@@ -87,7 +99,7 @@
                                                                     <span class="d-flex p-3 aiz-megabox-elem">
                                                                         <span
                                                                             class="aiz-rounded-check flex-shrink-0 mt-1"></span>
-                                                                        <span class="flex-grow-1 pl-3 text-left">
+                                                                        <span class="flex-grow-1 pl-2 text-left">
                                                                             <div>
                                                                                 <span
                                                                                     class="opacity-60">{{ translate('Address') }}:</span>
@@ -127,7 +139,7 @@
                                                                         </span>
                                                                     </span>
                                                                 </label>
-                                                                <div class="dropdown position-absolute right-0 top-0">
+                                                                <div class="dropdown position-absolute right-20 top-15">
                                                                     <button class="btn bg-gray px-2" type="button"
                                                                         data-toggle="dropdown">
                                                                         <i class="la la-ellipsis-v"></i>
@@ -143,13 +155,17 @@
                                                             </div>
                                                         @endforeach
                                                     @endif
+
+                                                     </div>
+                                                     </div>
                                                     <input type="hidden" name="checkout_type" value="logged">
-                                                    <div class="col-md-6 mx-auto mb-3">
-                                                        <div class="border p-3 rounded mb-3 c-pointer text-center bg-white h-100 d-flex flex-column justify-content-center"
+                                                    <div class="col-md-2 pl-3 pt-2">
+                                                        <div class="border p-3 rounded mb-3 c-pointer text-center bg-white"
                                                             onclick="add_new_address()">
                                                             <i class="las la-plus la-2x mb-3"></i>
                                                             <div class="alpha-7">{{ translate('Add New Address') }}</div>
                                                         </div>
+                                                    
                                                     </div>
                                                 </div>
                                             </div>
@@ -191,7 +207,7 @@
                             </div>
                             <div id="collapseDeliveryInfo" class="collapse show" aria-labelledby="headingDeliveryInfo"
                                 data-parent="#accordioncCheckoutInfo">
-                                <div class="card-body" id="delivery_info">
+                                <div class="card-body py-0" id="delivery_info">
                                     <form class="form-default delivery-type-form"
                                         action="{{ route('checkout.store_delivery_info') }}" role="form"
                                         method="POST">
@@ -228,7 +244,7 @@
                                                     <h5 class="fs-16 fw-600 mb-0">{{ get_setting('site_name') }}
                                                         {{ translate('Products') }}</h5>
                                                 </div>
-                                                <div class="card-body">
+                                                <div class="card-body p-0">
                                                     <ul class="list-group list-group-flush">
                                                         @php
                                                             $physical = false;
