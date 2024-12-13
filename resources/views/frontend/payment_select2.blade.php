a0f52cec3c712a71f98cbf65263b12d8e39d61ea
tiddukhan5682@gmail.com <tiddukhan5682@gmail.com>
2024-12-13 14:44:23 +0530

    Razorpay icon resize and redirect issue onclose fix

--- resources/views/frontend/payment_select2.blade.php
+++ resources/views/frontend/payment_select2.blade.php
@@ -975,13 +975,13 @@
                                                                 </div>
                                                             @endif
                                                             @if (get_setting('razorpay') == 1)
-                                                                <div class="col-6 col-md-4">
+                                                                <div class="col-6 col-md-3">
                                                                     <label class="aiz-megabox d-block mb-3">
                                                                         <input value="razorpay" class="online_payment"
                                                                             type="radio" name="payment_option" checked>
-                                                                        <span class="d-block aiz-megabox-elem p-3">
+                                                                        <span class="d-block aiz-megabox-elem p-2">
                                                                             <img src="{{ static_asset('assets/img/cards/rozarpay.png') }}"
-                                                                                class="img-fluid mb-2">
+                                                                                class="img-fluid mb-2 w-110">
                                                                             <span class="d-block text-center">
                                                                                 <span
                                                                                     class="d-block fw-600 fs-15">{{ translate('Razorpay') }}</span>
