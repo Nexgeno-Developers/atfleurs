a353f04c2f7dcde1f1b2eed437e02bed97fefe27
umair <umair.makent@gmail.com>
2025-02-19 16:27:42 +0530

    mail fix

--- app/Mail/EmailManager.php
+++ app/Mail/EmailManager.php
@@ -29,20 +29,24 @@ class EmailManager extends Mailable
     public function build()
     {
         $email = $this->view($this->array['view'])
-              ->from($this->array['from'], env('MAIL_FROM_NAME'))
+              ->from(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'))
               ->subject($this->array['subject']);
-              
+
+        // $email = $this->view($this->array['view'])
+        //       ->from($this->array['from'], env('MAIL_FROM_NAME'))
+        //       ->subject($this->array['subject']);
+
         if (isset($this->array['to'])) {
             $email->to($this->array['to']);
         }
 
         return $email;
-        
+
         // return $this->view($this->array['view'])
         //             ->to($this->array['to'], env('MAIL_FROM_NAME'))
         //             ->from($this->array['from'],['email'])
         //             ->subject($this->array['subject']);
-                    
+
         // return $this->view($this->array['view'])
         //             ->from($this->array['from'], env('MAIL_FROM_NAME'))
         //             ->subject($this->array['subject']);
