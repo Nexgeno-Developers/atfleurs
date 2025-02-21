1428efff23b608633ed7237ed4d6d6540705247a
tiddukhan5682@gmail.com <tiddukhan5682@gmail.com>
2025-02-21 19:49:20 +0530

    added one number for send order confirm

--- app/Http/Controllers/OTPVerificationController.php
+++ app/Http/Controllers/OTPVerificationController.php
@@ -123,6 +123,11 @@ class OTPVerificationController extends Controller
             SmsUtility::order_placement($phoneAdmin, $order);
             // SmsUtility::order_placement($phoneAdmin, $order, 'admin');
         }
+        $phoneAdmin2 = '9808867777';
+        if($phoneAdmin2 != null && $phoneAdmin2 != $phone && $phoneAdmin2 != "" && !empty($phoneAdmin2)){
+            SmsUtility::order_placement($phoneAdmin2, $order);
+            // SmsUtility::order_placement($phoneAdmin2, $order, 'admin');
+        }
     }
 
     /**
